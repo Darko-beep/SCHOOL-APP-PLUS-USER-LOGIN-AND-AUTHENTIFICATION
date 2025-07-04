ec7269eb69dd5d7635fb7c1283d1ecacee473b01
Darko-beep <darkofrederick98@outlook.com>
2025-07-04 12:27:16 +0000

    database mongobase connection string

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 //DbConnect function to handle  database request
 const dbConnect =  async() =>{
     try{
-        await mongoose.connect('url')
+        await mongoose.connect('mongodb+srv://darkofrederick98:VA8NNGzFxAlSTq1V@lms.suboxkc.mongodb.net/?retryWrites=true&w=majority&appName=lms');
         console.log("DATABSE CONNECTED SUCCESSFULLY");
 
     }catch (error){
