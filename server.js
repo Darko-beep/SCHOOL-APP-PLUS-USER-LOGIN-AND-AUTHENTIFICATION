08ff507daeeeaaf45929480a8370ce4807efa184
Darko-beep <darkofrederick98@outlook.com>
2025-07-05 02:30:41 +0000

    custom middlewares

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-require('dotenv').config();//dotenv for environment variables
-
-const http = require('http');
-require('./config/dbConnect'); // Import the database connection file
-
-//app import
-const app = require('./app/app');
-
-//create a port 
+require("dotenv").config();
+const http = require("http");
+require("./config/dbConnect");
+const app = require("./app/app");
 const PORT = process.env.PORT || 2020;
-
-
-
-// create an advanced server using http module
-const server =http.createServer(app);
-
-//server 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-// //console out the app
-// console.log(app);
+//server
+const server = http.createServer(app);
+server.listen(PORT, console.log(`Server is running on port ${PORT}`));
