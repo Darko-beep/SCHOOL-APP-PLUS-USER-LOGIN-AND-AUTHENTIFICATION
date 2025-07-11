1ffc3342997eb801cde4be6ab67bdc59b4a23ad3
Darko-beep <darkofrederick98@outlook.com>
2025-07-11 16:38:02 +0000

    school Academic Year controller done

--- app/app.js
+++ app/app.js
@@ -5,8 +5,10 @@ const {
   globalErrHandler,
   notFoundErr,
 } = require("../middlewares/globalErrHandler");
+// const  academicYearRouter = require("../routes/academics/academicYear");
 const morgan = require("morgan");
 const adminRouter = require("../routes/staff/adminRouter");
+const academicYearRouter = require("../routes/academics/academicYear");
 
 const app = express();
 
@@ -15,44 +17,12 @@ app.use(morgan("dev"));
 app.use (express.json()); // Parse JSON bodies
 
 
-// app.use((req, res, next) => {
-//   console.log(`${req.method} ${req.originalUrl}`);
-//   next();
-// });
-
-// let user = {
-//   name: "John Does",
-//   isAdmin: false,
-//   isLogin: true,
-// };
-
-// const isLogin = (req, res, next) => {
-//   if (user.isLogin) {
-//     next();
-//   } else {
-//     res.status(401).json({
-//       msg: "Unauthorized",
-//     });
-//   }
-// };
-
-// const isAdmin = (req, res, next) => {
-//   if (user.isAdmin) {
-//     next();
-//   } else {
-//     res.status(401).json({
-//       msg: "Unauthorized, you are not admin",
-//     });
-//   }
-// };
-
-// app.use(isLogin, isAdmin);
-
 
 
 //Routes
 //admin register
 app.use("/api/v1/admins", adminRouter);
+app.use("/api/v1/academic-years", academicYearRouter);
 
 
 //Error middlewares
